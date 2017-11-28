fea5d12696c5422b43bc4b98351de3cf9f8d4198
Pol <polbene95@gmail.com>
2017-11-28 10:53:30 +0100

    task3 done

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -3,38 +3,61 @@ $(document).ready(function () {
         data = json;
         console.log(data);
         crateGrid();
+        displayPlayers();
     });
 });
 
 
 function crateGrid() {
-    var table = document.getElementById("table-grid");
+    var yourGrid = document.getElementById("table-grid-your");
+    var enemyGrid = document.getElementById("table-grid-enemy");
     var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
     var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
     var ships = data.ships;
     for (i = 0; i < alphabet.length; i++) {
-        var row = document.createElement("tr");
+        var rowU = document.createElement("tr");
+        var rowE = document.createElement("tr");
         for (j = 0; j < numbers.length; j++) {
-            var col = document.createElement("td");
-            row.appendChild(col);
-            col.id = "U" + alphabet[i] + numbers[j];
-            col.innerHTML = alphabet[i] + numbers[j];
+            var colU = document.createElement("td");
+            var colE = document.createElement("td");
+            rowU.appendChild(colU);
+            rowE.appendChild(colE);
+            colU.id = "U" + alphabet[i] + numbers[j];
+            colE.id = "E" + alphabet[i] + numbers[j];
+            colU.innerHTML = alphabet[i] + numbers[j];
+            colE.innerHTML = alphabet[i] + numbers[j];
             for (k = 0; k < ships.length; k++) {
                 for (l = 0; l < ships[k].location.length; l++) {
                     if (ships[k].location[l] == alphabet[i] + numbers[j]) {
-                        if (ships[k].location.length == 1) {
-                            col.setAttribute("class", "boat-location");
-                        }
-                        if (ships[k].location.length == 2) {
-                            col.setAttribute("class", "cruiser-location");
-                        }
-                        if (ships[k].location.length == 3) {
-                            col.setAttribute("class", "destructor-location");
-                        }
+                        colU.setAttribute("class", "ship-location");
                     }
                 }
             }
         }
-        table.appendChild(row);
+        yourGrid.appendChild(rowU);
+        enemyGrid.appendChild(rowE);
     }
+}
+
+function displayPlayers() {
+    var playersDiv = document.getElementById("players");
+    var gameplayers = data.gameplayers;
+    var h2You = document.createElement("h2");
+    var h2Enemy = document.createElement("h2");
+    for (i = 0; i < gameplayers.length; i++) {
+        if (gameplayers[i].id == data.gpid) {
+            var playerYou = gameplayers[i].player.email
+        } else {
+            var playerEnemy = gameplayers[i].player.email
+        }
+    }
+
+    h2You.innerHTML = playerYou + "(you)";
+    h2Enemy.innerHTML = "(enemy)" + playerEnemy;
+    playersDiv.appendChild(h2You);
+    playersDiv.appendChild(h2Enemy);
+
+    console.log(playerYou);
+    console.log(playerEnemy);
+    console.log(data.gpid);
 }
